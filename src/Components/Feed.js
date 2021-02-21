8ad1b9389bad7ccf5466923cb56d40974a2a731e
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-02-21 01:34:43 +0100

    isolated form field into a component | linked form to firestore

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,29 +1,24 @@
 import React, { useState, useEffect }  from 'react';
 import './Feed.css';
-import CreateIcon from '@material-ui/icons/Create';
 import ImageIcon from '@material-ui/icons/Image';
 import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
 import EventIcon from '@material-ui/icons/Event';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 import FeedItem from './FeedItem';
 
-const Feed = () => {
+import FeedInputForm from './FeedInputForm';
 
-    const [input, setInput] = useState('');
+const Feed = () => {
     const [feed, setFeed] = useState([]);
 
     useEffect(() => {
         feed.length > 0 ? document.title = `(${feed.length}) Feed | LinkedIn` :  document.title = `LinkedIn`;
     }, [feed])
 
-    const handleFeedUpdate = (e) => {
-        e.preventDefault();
-        if(input){
-            const newFeed = [{input, id: new Date().getTime().toString()} ,...feed]
-            setFeed(newFeed);
-            setInput("");
-        }
-    }
+    // useEffect(() => {
+    //     effect
+    // }, [input])
+
 
     const handleFeedDelete = (id) => {
         const newFeed = feed.filter(feedItem => feedItem.id !== id)
@@ -41,16 +36,8 @@ const Feed = () => {
 
     return (
         <section className='post'>
-             <div className="post__topContents">
-                <div className="post__input">
-                    <CreateIcon />
-                    <form onSubmit={handleFeedUpdate}>
-                        <div className="post__input-form">
-                            <input type="text" placeholder="Start a post" value={input} onChange={(e) => setInput(e.target.value)}/>
-                        </div>
-                        {/* <button type="submit" style={{display: "none" }}>POST</button> */}
-                    </form>
-                </div>
+            <div className="post__topContents">
+                <FeedInputForm />
                 <div className="post__buttons">
                     {postButton((ImageIcon), "Photo")}
                     {postButton((VideoLibraryIcon), "Video")}
---
+++ src/Components/FeedInputForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './FeedInputForm.css';
+import CreateIcon from '@material-ui/icons/Create';
+import firebase from 'firebase';
+import { projectFirestore } from '../Config/firebase';
+
+const FeedInputForm = () => {
+    const [input, setInput] = useState('');
+
+    
+    const handleFeedUpdate = (e) => {
+        e.preventDefault();
+        if(input){
+            // const newFeed = [{input, id: new Date().getTime().toString()} ,...feed]
+            projectFirestore.collection('feed').add({
+                text: input,
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                id: new Date().getTime().toString(),
+            })
+            // setFeed(newFeed);
+            setInput("");
+        }
+    }
+
+    return (
+        <div className="post__input">
+            <CreateIcon />
+            <form onSubmit={handleFeedUpdate}>
+                <div className="post__input-form">
+                    <input type="text" placeholder="Start a post" value={input} onChange={(e) => setInput(e.target.value)}/>
+                </div>
+                {/* <button type="submit" style={{display: "none" }}>POST</button> */}
+            </form>
+        </div>
+    )
+}
+
+export default FeedInputForm
