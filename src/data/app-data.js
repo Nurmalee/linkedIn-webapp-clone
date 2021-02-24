51bc8b4cd60753ba2115e7ba3006d4a7adecbd0b
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-02-24 20:37:59 +0100

    added more hd pictures to widgetright ads array - opacity on hover

--- src/data/app-data.js
+++ src/data/app-data.js
@@ -41,9 +41,15 @@ export const headerNavaData = [
 
 export const widgetAdsPics = [
     'https://images.unsplash.com/photo-1596720426673-e4e14290f0cc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8bGlua2VkaW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1507297044195-4a5b6c75b7e4?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTh8fGxpbmtlZGlufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1507209696998-3c532be9b2b5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjB8fGxpbmtlZGlufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1597075433223-1e28a37391f3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjJ8fGxpbmtlZGlufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1603994466548-9fe139abe1dc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8bGlua2VkaW4lMjBwcm9maWxlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1460661419201-fd4cecdf8a8b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8YXJ0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1522932467653-e48f79727abf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fGNhcnN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     'https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjN8fGphdmFzY3JpcHR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1579403124614-197f69d8187b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OTN8fGphdmFzY3JpcHR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    'https://images.unsplash.com/photo-1549887552-cb1071d3e5ca?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjl8fGFydHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1501472312651-726afe119ff1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzN8fGFydHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1453856908826-6bbeda0f8490?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDR8fGNhcnN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1536685712909-6ac3633e0812?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDN8fGNhcnN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1493031534415-e40b830b1099?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzF8fGNhcnN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    
+
 ]
