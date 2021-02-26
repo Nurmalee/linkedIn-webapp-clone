419e6d1117515bcbe55e9196f3a332fdcd3f22a8
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-02-26 19:20:04 +0100

    UNINSTALLED REDUX - version discrepancies, switvhing to REACT CONTEXT

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import HeaderNav from './Components/HeaderNav'
 import Sidebar from './Components/Sidebar';
 import Feed from './Components/Feed';
 import WidgetRight from './Components/WidgetRight';
+import LoginPage from './Components/LoginPage';
 
 function App() {
+  const user = "lee"
   return (
     <div className="App">
       <HeaderNav />
-      <div className="app__content">
-        <Sidebar />
-        <Feed />
-        <WidgetRight />
-      </div>
+        {!user? <LoginPage /> : 
+          (
+            <div className="app__content">
+              <Sidebar />
+              <Feed />
+              <WidgetRight />
+            </div>
+          )
+        }
     </div>
   );
 }
